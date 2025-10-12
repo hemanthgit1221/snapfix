f9a6a1f73c1b99211db951cadf4404dcebd53d08
Hemanth Kumar <hemanthfas1727@gmail.com>
2025-10-12 12:13:13 +0530

    Fix linting warning: Remove unused PasswordEncoder field from AuthController
    - Removed unused passwordEncoder field and import
    - Fixed linting warning about unused field
    - Backend rebuilt with Docker to apply changes

--- backend/src/main/java/com/snapfix/controller/AuthController.java
+++ backend/src/main/java/com/snapfix/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.snapfix.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +16,6 @@ public class AuthController {
     
     @Autowired
     private UserService userService;
-    
-    @Autowired
-    private PasswordEncoder passwordEncoder;
     
     @Autowired
     private JwtService jwtService;
